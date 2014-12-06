f1e5e163543a4c1c3ac7c77b64dd8ba6dd8572ce
Brian <blee29@ucsc.edu>
2014-12-06 14:24:12 -0800

    A small change to menu link.

--- models/a_menu.py
+++ models/a_menu.py
@@ -6,7 +6,7 @@
 #########################################################################
 
 response.logo = A(B('EventCentral'),XML('&trade;&nbsp;'),
-                  _class="brand",_href="http://www.web2py.com/")
+                  _class="brand",_href=URL('default','index'))
 response.title = request.application.replace('_',' ').title()
 response.subtitle = ''
 
