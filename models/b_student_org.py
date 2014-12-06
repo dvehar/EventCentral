3c15ff4d16ee2361e68ab36471fc88aa18366699
Desmond Vehar <dvehar@ucsc.edu>
2014-12-06 13:53:16 -0800

    Fixed the student org table. Added Notification Table.

--- models/b_student_org.py
+++ models/b_student_org.py
@@ -10,13 +10,10 @@ db.define_table('student_org',
                 Field('meeting_location', 'string', requires=IS_NOT_EMPTY()),
                 Field('meeting_times', 'text', requires=IS_NOT_EMPTY()),
                 Field('website', 'string', requires=IS_NOT_EMPTY()),
-                Field('email', 'string', requires=IS_NOT_EMPTY()),
-                Field('leaders', 'string', requires=IS_NOT_EMPTY()),
-                Field('picture', 'upload', requires=IS_NOT_EMPTY()),
-                Field('admins', 'reference auth_user', default=auth.user_id)  #For debugging purposes. Functionality will be replaced by admin_pool fields.
+                Field('email', 'string', requires=IS_NOT_EMPTY())
                 )
 
 db.student_org.join_date.writable = db.student_org.join_date.readable = False
 db.student_org.name.requires = IS_NOT_IN_DB(db, db.student_org.name)   #Added this line to ensure name doesnt already exist in db.
 db.student_org.id.readable = False
-db.student_org.admins.writable = db.student_org.admins.readable = False   #For debugging purposes.
+
